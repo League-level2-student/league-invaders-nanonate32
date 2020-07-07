a36011297cf2ac6c7ebe7cafa2c06c2b5aed2ade
mvish <mvish@MishaNGS-PC.lan>
2020-07-06 19:57:36 -0700

    LeagueInvaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,4 +1,10 @@
+import javax.swing.JFrame;
 
 public class LeagueInvaders {
-
+JFrame frame = new JFrame();
+final int WIDTH = 500;
+final int HEIGHT = 800;
+public static void main(String[] args) {
+	void setupp
+}
 }
