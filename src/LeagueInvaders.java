35abcb7a8ee55828f24c21134c16e44b345b28fa
mvish <mvish@MishaNGS-PC.lan>
2020-07-20 13:28:05 -0700

    LeagueInvaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,8 +1,11 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class LeagueInvaders {
 	
-	leagueInvaders(int invader = invader.this );
+	public LeagueInvaders(LeagueIvaders.this, name.this){
+		// TODO Auto-generated constructor stub
+	}
 JFrame frame;
 JPanel panel = new JPanel();
 final int WIDTH = 500;
@@ -10,6 +13,8 @@ final int HEIGHT = 800;
 public static void main(String[] args) {
 	frame.setVisible(true);
 	JButton button = new JButton();
+	setDefualtCloseOperation(frame.EXIT_ON_CLOSE)
+	LeagueInvaders LeagueInvaders = new LeagueInvaders
 }
 	void setup() {
 		
