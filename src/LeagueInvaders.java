cdc886d5e0720730388764f6548b29b06af4bf1b
mvish <mvish@MishaNGS-PC.lan>
2020-07-21 13:28:29 -0700

    leagueinvaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,3 +1,5 @@
+import java.awt.Frame;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
@@ -6,17 +8,17 @@ public class LeagueInvaders {
 	public LeagueInvaders(LeagueIvaders.this, name.this){
 		// TODO Auto-generated constructor stub
 	}
-JFrame frame;
+JFrame frame = new JFrame();
 JPanel panel = new JPanel();
 final int WIDTH = 500;
 final int HEIGHT = 800;
 public static void main(String[] args) {
-	frame.setVisible(true);
-	JButton button = new JButton();
-	setDefualtCloseOperation(frame.EXIT_ON_CLOSE)
-	LeagueInvaders LeagueInvaders = new LeagueInvaders
+	
+	
+	
 }
 	void setup() {
-		
+    	frame.setvisible();
+    	frame.setAddActionListener
 }
 }
