2e344d18b76ec7bb327ad3678cd6e062ab9238a9
mvish <mvish@MishaNGS-PC.lan>
2020-07-22 13:30:23 -0700

    LeagueInvaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -6,19 +6,20 @@ import javax.swing.JFrame;
 public class LeagueInvaders {
 	
 	public LeagueInvaders(LeagueIvaders.this, name.this){
-		// TODO Auto-generated constructor stub
-	}
 JFrame frame = new JFrame();
 JPanel panel = new JPanel();
 final int WIDTH = 500;
 final int HEIGHT = 800;
 public static void main(String[] args) {
 	
+	}
+
 	
 	
 }
 	void setup() {
     	frame.setvisible();
-    	frame.setAddActionListener
+    	frame.setDefualtCloseOperation(frame.EXIT_ON_CLOSE)
+    	frame.setObject();
 }
 }
