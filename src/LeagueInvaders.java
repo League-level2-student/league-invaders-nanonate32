5f84a0764ced1002d2987569ac672815e9e511de
mvish <mvish@MishaNGS-PC.lan>
2020-07-23 13:25:18 -0700

    LeagueInvaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -2,24 +2,26 @@ import java.awt.Frame;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class LeagueInvaders {
-	
-	public LeagueInvaders(LeagueIvaders.this, name.this){
-JFrame frame = new JFrame();
-JPanel panel = new JPanel();
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	public LeagueInvaders(){
+
 final int WIDTH = 500;
 final int HEIGHT = 800;
+	}
 public static void main(String[] args) {
 	
 	}
 
 	
 	
-}
+
 	void setup() {
-    	frame.setvisible();
-    	frame.setDefualtCloseOperation(frame.EXIT_ON_CLOSE)
-    	frame.setObject();
+    	frame.setVisible(true);
+    	frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+    	frame.pack();
 }
 }
---
+++ src/gamePanel.java
@@ -0,0 +1,19 @@
+
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class gamePanel extends JPanel implements ActionListener {
+Timer timer = new Timer(2, null);
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+	}
+
+
