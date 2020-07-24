5c040c7fb80177896881329e4756c51c61c36a9c
mvish <mvish@MishaNGS-PC.lan>
2020-07-24 13:27:51 -0700

    LeagueInvaders

--- src/gamePanel.java
+++ src/gamePanel.java
@@ -7,12 +7,15 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class gamePanel extends JPanel implements ActionListener {
+	public gamePanel(1000/10{
+		// TODO Auto-generated constructor stub
+	}
 Timer timer = new Timer(2, null);
 
 @Override
 public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
-	
+	System.out.println("Hello");
 }
 	}
 
