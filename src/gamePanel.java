30ea78dd0772c94372e88668528e6cf5b27f766d
mvish <mvish@MishaNGS-PC.lan>
2020-07-29 16:57:07 -0700

    gamePanel

--- src/gamePanel.java
+++ src/gamePanel.java
@@ -7,15 +7,17 @@ import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class gamePanel extends JPanel implements ActionListener {
-	public gamePanel(1000/10{
+	public gamePanel(
 		// TODO Auto-generated constructor stub
 	}
 Timer timer = new Timer(2, null);
-
+timer.start();
 @Override
 public void actionPerformed(ActionEvent e) {
 	// TODO Auto-generated method stub
 	System.out.println("Hello");
+	void startGame(){
+		
 }
 	}
 
