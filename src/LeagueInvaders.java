2cafba8405f501a53b5453e384b72b14dca41464
mvish <mvish@MishaNGS-PC.lan>
2020-08-29 17:32:54 -0700

    leagueInvaders

--- src/LeagueInvaders.java
+++ src/LeagueInvaders.java
@@ -1,3 +1,4 @@
+import java.awt.Dimension;
 import java.awt.Frame;
 
 import javax.swing.JButton;
@@ -10,20 +11,26 @@ public class LeagueInvaders {
 	gamePanel game;
 	public LeagueInvaders(){
     game = new gamePanel();
-final int WIDTH = 500;
+    final int WIDTH = 500;
 final int HEIGHT = 800;
+    frame.getContentPane().setPreferredSize(new Dimension(WIDTH, HEIGHT));
+     
+    frame.pack();
+
 	}
 public static void main(String[] args) {
-	
+	LeagueInvaders invaders = new LeagueInvaders();
+	invaders.setup();
 	}
 
 	
 	
-
-	void setup() {
-		frame.add(gamePanel);
+    
+	 void setup() {
+		frame.add(game);
     	frame.setVisible(true);
     	frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
     	frame.pack();
+    	game.startGame();
 }
 }
