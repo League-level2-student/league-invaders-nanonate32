b9b6c4c675a73520a669e968769eb03de69ac62e
mvish <mvish@MishaNGS-PC.lan>
2020-09-19 17:26:40 -0700

    leagueInvaders

--- src/gamePanel.java
+++ src/gamePanel.java
@@ -3,10 +3,11 @@
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 
 import javax.swing.JPanel;
 import javax.swing.Timer;
-public class gamePanel extends JPanel implements ActionListener {
+public class gamePanel extends JPanel implements ActionListener, KeyListener{
 	  public gamePanel() {
 		timer = new Timer(1000/60,this);
 	  }
@@ -19,12 +20,12 @@ public class gamePanel extends JPanel implements ActionListener {
 
 @Override
 public void actionPerformed(ActionEvent e) {
-	// TODO Auto-generated method stub
+	// TODO Autotim-generated method stub
 	System.out.println("Hello");
 }
 	void startGame(){
 	timer.start();	
-		timer.
+	
 }
 }	
 
